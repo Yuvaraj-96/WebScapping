3a8968632ae47c79c175afd70d9c38d7d2b18a8b
Yuvaraj-96 <yuvarajmechmailam@gmail.com>
2022-02-13 14:14:02 +0530

    Second  commited

--- src/Scrapper.js
+++ src/Scrapper.js
@@ -16,54 +16,24 @@ function Scrapper() {
 
 
 
-// /* ... */
-// // cron.schedule('0 */12 * * *', async () => {
-//   console.log('running a task every hour ⏲️');
-//   const html = await getHTML(productPage).catch(console.log);
-//   const currentPrice = currencyStringToNumber(scrapPrice(html));
-//   if (currentPrice < desiredPrice) {
-//     console.log('Congratulations! you just saved some bucks 💵');
-//   }
-// // });
-
-
-// function scrapPrice(html) {
-//     const $ = cheerio.load(html); //First you need to load in the HTML
-//     const price = $(selector)
-//       .text() // we get the text
-//       .trim();
-//       return price;
-//   }
-
-
-// async function rawHTML (urls) {
-//     const { data: html } = await axios.get(urls).catch(() => {
-//       console.log("Couldn't get the page ☹️")
-//     })
-//     return html
-//   }
-
-
-
-//git adds
 
 async function fetchdetails(){
-try{
+ try{
 
-const response = await axios.get("https://www.amazon.com/s?k=mobile&crid=2EJZBZWHTVS7R&sprefix=mobile%2Caps%2C4256&ref=nb_sb_noss",{headers: {"Access-Control-Allow-Origin": "http://localhost:3000/",'Access-Control-Allow-Credentials': 'true',"Access-Control-Allow-Methods":"GET, OPTIONS, POST, PUT"}});
-const htmlpage = response.data;
-const $ = cheerio.load(htmlpage);
+// const response = await axios.get("https://www.amazon.com/s?k=mobile&crid=2EJZBZWHTVS7R&sprefix=mobile%2Caps%2C4256&ref=nb_sb_noss",{headers: {"Access-Control-Allow-Origin": "http://localhost:3000/",'Access-Control-Allow-Credentials': 'true',"Access-Control-Allow-Methods":"GET, OPTIONS, POST, PUT"}});
+// const htmlpage = response.data;
+// const $ = cheerio.load(htmlpage);
 
-const product =[];
+// const product =[];
 
-$('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20').each((_idx, el) => {
-    const prod = $(el)
-    const title = prod.find('span.a-size-base-plus.a-color-base.a-text-normal').text()
-console.log(title)
-    product.push(title)
-});
+// $('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20').each((_idx, el) => {
+//     const prod = $(el)
+//     const title = prod.find('span.a-size-base-plus.a-color-base.a-text-normal').text()
+// console.log(title)
+//     product.push(title)
+// });
 
-return product;
+// return product;
 
 }catch(err)
 {
--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import Scapper from './Scrapper';
-// import Header from './Header'
-// import Scrapper from './Scrapper';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Scapper from './Scrapper';
+import Header from './Header'
+import Scrapper from './Scrapper';
 
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//   <Header/> 
-//   <Scapper/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+  <Header/> 
+  <Scapper/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
 
 
-const http= require('http');
-const https= require('https');
-const express = require('express');
+// const http= require('http');
+// const https= require('https');
+// const express = require('express');
 
-const port =3000;
-const host ='localhost'
-const {amazon} = require('./amazonScrapper');
+// const port =3000;
+// const host ='localhost'
+// const {amazon} = require('./amazonScrapper');
 
-const app = express();
+// const app = express();
 
-const start=()=>{
-  console.log('Amazon Scrapping is running');
-  amazon();
-}
+// const start=()=>{
+//   console.log('Amazon Scrapping is running');
+//   amazon();
+// }
 
-const server = https.createServer(app);
+// const server = https.createServer(app);
 
-server.listen(port,host,()=>{
-  console.log("server is running at "+host+":"+port)
-  start();
-})
+// server.listen(port,host,()=>{
+//   console.log("server is running at "+host+":"+port)
+//   start();
+// })
 
 
 
